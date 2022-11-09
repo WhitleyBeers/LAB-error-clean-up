27ad9933cf696ace63bd9d6d24bc37df478f8958
Whitley Beers <whitleybeers@gmail.com>
2022-11-08 21:28:43 -0600

    modularized code

---
+++ utils/sortStudent.js
@@ -0,0 +1,25 @@
+import studentsOnDom from './studentsOnDom';
+import { students, houses } from './data/studentData';
+import createId from './createId';
+
+const sortStudent = (e) => {
+  e.preventDefault();
+  const sortingHat = houses[Math.floor(Math.random() * houses.length)];
+
+  if (e.target.id === 'sorting') {
+    const student = document.querySelector('#student-name');
+
+    // create the new student object
+    students.push({
+      id: createId(students),
+      name: student.value,
+      house: sortingHat.house,
+      crest: sortingHat.crest
+    });
+
+    student.value = ''; // reset value of input
+    studentsOnDom('#students', students);
+  }
+};
+
+export default sortStudent;
